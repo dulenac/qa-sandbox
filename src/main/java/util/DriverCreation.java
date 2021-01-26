f86d7709c8b5e6007ad93521b69bf519f9b5893b
dulenac <todorovic.kg@gmail.com>
2021-01-26 16:50:03 +0100

    Edit pathToChromeDriverLocation

--- src/main/java/util/DriverCreation.java
+++ src/main/java/util/DriverCreation.java
@@ -43,7 +43,7 @@ public class DriverCreation {
 
     private void createChromeDriver() {
 
-        System.setProperty("webdriver.chrome.driver", "/Users/dulenac/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "pathToYourChromdriverLocation");
         _driver = new ChromeDriver();
 
 
